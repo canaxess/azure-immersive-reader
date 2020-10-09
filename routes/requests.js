07643af9c070918473d8002b7cd24dcd13abd353
rossmullen <43454021+rossmullen@users.noreply.github.com>
2020-10-10 06:47:46 +1100

    added new API routes and logic

--- routes/requests.js
+++ routes/requests.js
@@ -29,4 +29,35 @@ router.get('/subdomain', function (req, res) {
     return res.send(process.env.SUBDOMAIN);
 })
 
+router.get('/renderframework', function (req, res) {
+    return res.send({ framework: process.env.FRAMEWORK });
+});
+
+router.get('/setframework', function (req, res) {
+    //only sets the ENV for the process
+    let frameworkRequest = req.query.FRAMEWORK;
+    
+    if(typeof frameworkRequest !== 'undefined')
+    {
+        switch (frameworkRequest.toLowerCase()) 
+        {
+            case 'react':
+                console.log(`REACT requested`);
+                process.env.FRAMEWORK = "REACT";
+                break;
+            case 'angular':
+                console.log(`ANGULAR requested`);
+                process.env.FRAMEWORK = "ANGULAR";
+                break;
+            case 'vue':
+                console.log(`VUE requested`);
+                process.env.FRAMEWORK = "VUE";
+                break;
+            default:
+                console.log(`not an allowed framework [${frameworkRequest}]`);
+        }
+    }
+    return res.sendStatus(200)
+});
+
 module.exports = router;
